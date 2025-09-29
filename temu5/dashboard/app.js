b5e12ac0b1d6a21c7e0301a10eea1695c1925be6
Rimuru <filbertasherdarius@gmail.com>
2025-09-29 11:21:50 +0700

    pertemuan5

---
+++ temu5/dashboard/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ temu5/dashboard/app1.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+let product = [];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/minimarket', (req, res) => {
+    res.render('minimarket', { product });  
+});
+
+app.get("/add", (req, res) => {
+    res.render("form");
+});
+
+app.post("/add", (req, res) => {
+    const { name, qty, price } = req.body;
+    product.push({ name, qty: parseInt(qty), price: parseFloat(price) });
+    res.redirect("/minimarket");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
